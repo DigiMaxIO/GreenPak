518233d22fbfd325aa4d56d55e8972a317a3ce51
DigiMax <42356489+DigiMaxIO@users.noreply.github.com>
2024-06-26 20:47:37 -0700

    Update Example2.ino

--- examples/Example2/Example2.ino
+++ examples/Example2/Example2.ino
@@ -1,24 +1,27 @@
-#include "Silego.h"             // Include Silego header file
+#include "Greenpak.h"             // Include Greenpak header file
 #include "macros/SLG46531.h"    // Include macros for SLG46531
 
-// Create an instance of Silego class called
-// "silego" with device address 0x00
-Silego silego(0x00);          
+// Create an instance of Greenpak class called
+// "greenpak" with device address 0x00
+Greenpak greenpak(0x00);          
 
 uint32_t a = 0x01;
 byte myData;
 
-void setup() {
+void setup() 
+{
 	Serial.begin(115200);
 	Serial.print("GreenPAK: ");
 	Serial.println(GreenPAK);
-	silego.writeI2C(RAM_BYTE_0,0x00);
+	greenpak.writeI2C(RAM_BYTE_0,0x00);
 }
 
-void loop() {
-	while(a <= 0xFF) {
-		silego.writeI2C(RAM_BYTE_0, 1, a);    // write "1" to bit_location "a" in RAM_BYTE_0
-		myData = silego.readI2C(RAM_BYTE_0);  // read data in RAM_BYTE_0 and store it in "myData"
+void loop() 
+{
+	while(a <= 0xFF) 
+	{
+		greenpak.writeI2C(RAM_BYTE_0, 1, a);    // write "1" to bit_location "a" in RAM_BYTE_0
+		myData = greenpak.readI2C(RAM_BYTE_0);  // read data in RAM_BYTE_0 and store it in "myData"
 
 		Serial.print("myData (BIN): ");       // print the value of "myData" to
 		Serial.print(myData, BIN);            // the Arduino Serial Monitor
